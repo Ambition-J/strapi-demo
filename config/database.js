6ad8db23e220d55ae7354329e878e51cbf87c094
吴疆 <ajiang@bogon.host>
2022-05-19 21:25:33 +0800

    init

---
+++ config/database.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: "mysql",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "blog"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "123456"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+      },
+    },
+    debug: false,
+  },
+});
